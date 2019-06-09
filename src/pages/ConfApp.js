6c87ce1a8f6005be78bfe04cc67feb7187f87e9e
Giancarlo Vera <gianverab@gmail.com>
2019-06-08 22:53:41 -0500

    add form component and handle user events

--- src/pages/ConfApp.js
+++ src/pages/ConfApp.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 import Badge from "../components/Badge";
+import BadgeForm from "../components/BadgeForm";
 import Navbar from "../components/Navbar";
 import header from "../images/badge-header.svg";
 import "./styles/ConfApp.css";
@@ -15,8 +16,11 @@ class ConfApp extends Component {
         </div>
         <div className="container">
           <div className="row">
-            <div className="col">
+            <div className="col-6">
               <Badge />
+            </div>
+            <div className="col-6">
+              <BadgeForm />
             </div>
           </div>
         </div>
